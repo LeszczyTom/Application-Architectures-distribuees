181473c49bc757b0e3c25f0b5c1fb7c63210b5f2
Tom <leszczytom@gmail.com>
2022-03-16 20:26:41 +0100

    Ajout TEst.java et Main.java

---
+++ NLP/src/com/tl/Main.java
@@ -0,0 +1,7 @@
+package com.tl;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Main");
+    }
+}
---
+++ NLP/src/test/tl/Test.java
@@ -0,0 +1,13 @@
+package test.tl;
+
+import com.tl.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+    @Test
+    public void playMusicByTitle() {
+
+    }
+}
