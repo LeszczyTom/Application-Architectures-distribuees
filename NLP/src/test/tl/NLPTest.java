6d14922aa4a019af15d10d05679d386027a65151
LeszczyTom <leszczytom@gmail.com>
2022-03-17 20:26:21 +0100

    Majuscule -> minuscule

--- NLP/src/test/tl/NLPTest.java
+++ NLP/src/test/tl/NLPTest.java
@@ -7,37 +7,37 @@ public class NLPTest {
 
     @org.junit.Test
     public void playMusic() {
-        assertEquals( new Result ("Play", "Come Out and Play").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Come out and play").toString());
-        assertEquals( new Result ("Play", "Chelsea Dagger").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Chelsea Dagger").toString());
-        assertEquals( new Result ("Play", "Attack").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Attack").toString());
-        assertEquals( new Result ("Play", "Toxicity").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Toxicity").toString());
-        assertEquals( new Result ("Play", "Room on fire").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Room on fire").toString());
-        assertEquals( new Result ("Play", "The offspring").toString(), nlp.coupleActionObjetDepuisPhrase("Joue The offspring").toString());
-        assertEquals( new Result ("Play", "Reptilia - The strokes").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Reptilia de The Strokes").toString());
-        assertEquals( new Result ("Play", "Come out and play - The offspring").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Come out and play de The offspring").toString());
+        assertEquals( new Result ("play", "come out and play").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Come out and play").toString());
+        assertEquals( new Result ("play", "chelsea dagger").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Chelsea Dagger").toString());
+        assertEquals( new Result ("play", "attack").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Attack").toString());
+        assertEquals( new Result ("play", "toxicity").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Toxicity").toString());
+        assertEquals( new Result ("play", "room on fire").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Room on fire").toString());
+        assertEquals( new Result ("play", "the offspring").toString(), nlp.coupleActionObjetDepuisPhrase("Joue The offspring").toString());
+        assertEquals( new Result ("play", "reptilia - the strokes").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Reptilia de The Strokes").toString());
+        assertEquals( new Result ("play", "come out and play - the offspring").toString(), nlp.coupleActionObjetDepuisPhrase("Joue Come out and play de The offspring").toString());
     }
 
 
     @org.junit.Test
     public void pauseMusic() {
-        assertEquals( new Result ("Pause").toString(), nlp.coupleActionObjetDepuisPhrase("Met sur pause").toString());
+        assertEquals( new Result ("pause").toString(), nlp.coupleActionObjetDepuisPhrase("Met sur pause").toString());
     }
 
     @org.junit.Test
     public void resumeMusic() {
-        assertEquals( new Result ("Resume").toString(), nlp.coupleActionObjetDepuisPhrase("Reprend la lecture").toString());
+        assertEquals( new Result ("resume").toString(), nlp.coupleActionObjetDepuisPhrase("Reprend la lecture").toString());
     }
 
     @org.junit.Test
     public void volume() {
-        assertEquals( new Result ("VolUp").toString(), nlp.coupleActionObjetDepuisPhrase("Baisse le son").toString());
-        assertEquals( new Result ("VolDown").toString(), nlp.coupleActionObjetDepuisPhrase("Monte le son").toString());
+        assertEquals( new Result ("volUp").toString(), nlp.coupleActionObjetDepuisPhrase("Baisse le son").toString());
+        assertEquals( new Result ("volDown").toString(), nlp.coupleActionObjetDepuisPhrase("Monte le son").toString());
     }
 
     @org.junit.Test
     public void like() {
-        Result like = new Result ("Like");
-        Result dislike = new Result ("Dislike");
+        Result like = new Result ("like");
+        Result dislike = new Result ("dislike");
 
         assertEquals( like.toString(), nlp.coupleActionObjetDepuisPhrase("J'aime ce morceau").toString());
         assertEquals( dislike.toString(), nlp.coupleActionObjetDepuisPhrase("Je n'aime pas ce morceau").toString());
@@ -47,26 +47,26 @@ public class NLPTest {
 
     @org.junit.Test
     public void skipForward() {
-        Result skipForward = new Result ("SkipForw");
+        Result skipForward = new Result ("forward");
         assertEquals( skipForward.toString(), nlp.coupleActionObjetDepuisPhrase("Passe ce morceau").toString());
         assertEquals( skipForward.toString(), nlp.coupleActionObjetDepuisPhrase("Suivant").toString());
     }
 
     @org.junit.Test
     public void skipBackward() {
-        Result skipBack = new Result ("SkipBack");
+        Result skipBack = new Result ("backward");
         assertEquals( skipBack.toString(), nlp.coupleActionObjetDepuisPhrase("Revient en arrière").toString());
         assertEquals( skipBack.toString(), nlp.coupleActionObjetDepuisPhrase("Precedent").toString());
     }
 
     @org.junit.Test
     public void shuffle() {
-        assertEquals( new Result ("Shuffle").toString(), nlp.coupleActionObjetDepuisPhrase("Met en boucle").toString());
+        assertEquals( new Result ("shuffle").toString(), nlp.coupleActionObjetDepuisPhrase("Met en boucle").toString());
     }
 
     @org.junit.Test
     public void stopMusic() {
-        Result stop = new Result ("Stop");
+        Result stop = new Result ("stop");
         assertEquals( stop.toString(), nlp.coupleActionObjetDepuisPhrase("Stop").toString());
         assertEquals( stop.toString(), nlp.coupleActionObjetDepuisPhrase("Arrête la musique").toString());
         assertEquals( stop.toString(), nlp.coupleActionObjetDepuisPhrase("Stop la musique").toString());
