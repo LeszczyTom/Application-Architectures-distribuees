e8405ca681a92761feace035d924ba8517acea2d
LeszczyTom <leszczytom@gmail.com>
2022-03-27 00:39:01 +0100

    UI terminé

--- AppAjoutMusique/index.js
+++ AppAjoutMusique/index.js
@@ -1,14 +1,18 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
-		width: 600,
-		height: 800,
+		width: 400,
+		height: 500,
+		transparent: true,
+		frame: false,
+		resizable:false,
+		maximizable:false,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js')
 		}
@@ -43,3 +47,8 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on('test', function (event, arg) {
+	console.log(arg)
+})
+console.log("e")
---
+++ AppAjoutMusique/renderer.js
@@ -0,0 +1,10 @@
+/*const ipc = require('electron').ipcRenderer;
+
+ipc.send('test', "RRRR")
+*/
+    /*let data = {
+        titre: document.getElementById("Titre").value,
+        artiste: document.getElementById("Artiste").value,
+        album: document.getElementById("Album").value,
+    }
+    ipcRenderer.send('submitForm', data);*/
