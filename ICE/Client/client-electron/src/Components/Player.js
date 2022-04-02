580c451efcefc1d2b9ce3c802c0124ce0d00c234
LeszczyTom <leszczytom@gmail.com>
2022-04-02 12:19:43 +0200

    Client stream http

--- ICE/Client/client-electron/src/Components/Player.js
+++ ICE/Client/client-electron/src/Components/Player.js
@@ -1,24 +1,19 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import backward from "../Ressources/first_page_white_24dp.svg"
 import forward from "../Ressources/last_page_white_24dp.svg"
-import jsmpeg from "jsmpeg"
 
 function Player() {
-
-    let client = new WebSocket('ws://localhost:8084');
-    new jsmpeg(client, {});
-
     return (
         <>
-            <div className="jsmpeg" data-url={client} data-loop="true" data-autoplay="true"/>
+            <video controls src={"http://127.0.0.1:5555"}/>
             <div className={"flex bg-neutral-800 w-full h-[90px] mb-0 rounded-t-2xl"}>
-                <img src={"https://picsum.photos/50"} className={"h-[50px] my-auto ml-5 rounded-xl"}/>
+                <img src={"https://picsum.photos/50"} className={"h-[50px] my-auto ml-5 rounded-xl"} alt={"album musique joué"}/>
                 <div className={"flex flex-col text-white my-auto ml-2 text-sm"}>
                     <p>Titre</p>
                     <p className={"text-gray-400"}>Artiste</p>
                 </div>
-                <img src={backward}/>
-                <img src={forward}/>
+                <img src={backward} alt={"icône precedent"}/>
+                <img src={forward} alt={"icône suivant"}/>
             </div>
         </>
     );
