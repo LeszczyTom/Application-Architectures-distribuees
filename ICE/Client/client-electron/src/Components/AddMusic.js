05d8cad052dfa095f23c14b5db51d18387f2150f
LeszczyTom <leszczytom@gmail.com>
2022-04-03 17:58:26 +0200

    Ajout méthode clenUpFileName

--- ICE/Client/client-electron/src/Components/AddMusic.js
+++ ICE/Client/client-electron/src/Components/AddMusic.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 function AddMusic() {
     const [file, setFile] = React.useState("");
+
+    const cleanUpFileName = (fileName) => {
+        let tmp = fileName.split("\\");
+        return tmp[tmp.length - 1];
+    }
+
     return (
         <div className={"h-full m-4 flex"}>
             <div className={"flex flex-col mx-[300px] p-4 my-auto text-white"}>
@@ -13,7 +19,7 @@ function AddMusic() {
                     <label htmlFor={"fileMusic"} className={"my-auto w-[180px] hover:cursor-pointer hover:text-[#1DB954]"}>Choisir un fichier MP3: </label>
                     <input id={"fileMusic"} type={"file"} className={"hidden"}  accept={".mp3"} onChange={(event) => setFile(event.target.value)} />
                     <div className={"w-[354px] h-[40px] border-2 border-neutral-600 rounded-lg px-3 my-auto bg-neutral-800 flex"}>
-                        <p className={"my-auto overflow-hidden whitespace-nowrap overflow-ellipsis"}>{file}</p>
+                        <p className={"my-auto overflow-hidden whitespace-nowrap overflow-ellipsis"}>{cleanUpFileName(file)}</p>
                     </div>
                 </div>
                 <input type={"submit"} value={"Ajouter la musique"} className={"mt-8 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 hover:border-[#1DB954] w-[50%] m-auto"}/>
