aaf12cfab3f3f3c797ed01509edbf3cb5168445d
LeszczyTom <leszczytom@gmail.com>
2022-04-03 14:15:20 +0200

    Ajout btns dans le component AddMusic

--- ICE/Client/client-electron/src/Components/AddMusic.js
+++ ICE/Client/client-electron/src/Components/AddMusic.js
@@ -8,6 +8,7 @@ function AddMusic() {
                 <input type={"text"} placeholder={"Titre"} className={"mt-4 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 focus:border-[#1DB954]"}/>
                 <input type={"text"} placeholder={"Artiste"} className={"mt-4 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 focus:border-[#1DB954]"}/>
                 <input type={"text"} placeholder={"Album"} className={"mt-4 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 focus:border-[#1DB954]"}/>
+                <input type={"text"} placeholder={"Lien de la pochette de l'album"} className={"mt-4 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 focus:border-[#1DB954]"}/>
                 <div className={"flex w-full mt-4"}>
                     <label htmlFor={"fileMusic"} className={"my-auto w-[180px] hover:cursor-pointer hover:text-[#1DB954]"}>Choisir un fichier MP3: </label>
                     <input id={"fileMusic"} type={"file"} className={"hidden"}  accept={".mp3"} onChange={(event) => setFile(event.target.value)} />
@@ -15,6 +16,7 @@ function AddMusic() {
                         <p className={"my-auto overflow-hidden whitespace-nowrap overflow-ellipsis"}>{file}</p>
                     </div>
                 </div>
+                <input type={"submit"} value={"Ajouter la musique"} className={"mt-8 h-[40px] bg-neutral-800 rounded-lg border-2 bg-black border border-neutral-600 px-3 hover:border-[#1DB954] w-[50%] m-auto"}/>
             </div>
         </div>
     );
